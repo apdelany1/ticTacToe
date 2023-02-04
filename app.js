d973608d7ac9188c54e96cff7e1d410ea4576aeb
aidan <apdelany1@gmail.com>
2023-02-04 12:49:31 -0500

    Added more styles, background, Itim font, and more gap between the cells, everything is prime for a tic tac toe logic to happen

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const mark = document.querySelectorAll(".cell");
 mark.forEach((element) => {
   element.addEventListener("click", () => {
     console.log("hello");
-    element.textContent = marker;
-    if (marker === "X") {
-      marker = "O";
-    } else {
-      marker = "X";
+    if (element.textContent === "") {
+      element.textContent = marker;
+      if (marker === "X") {
+        marker = "O";
+      } else {
+        marker = "X";
+      }
     }
+
   });
 });
