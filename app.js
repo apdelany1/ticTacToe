53e0a93bc94bdb0f284957ec5089152ccaf752a9
aidan <apdelany1@gmail.com>
2023-02-04 12:35:37 -0500

    added basic grid for tic tac toe and provided some styling. JavaScript was also added just to provide simple changing on the click. event listeners are in place

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Fudge")
+let marker = "X";
+
+const mark = document.querySelectorAll(".cell");
+
+mark.forEach((element) => {
+  element.addEventListener("click", () => {
+    console.log("hello");
+    element.textContent = marker;
+    if (marker === "X") {
+      marker = "O";
+    } else {
+      marker = "X";
+    }
+  });
+});
