948225b19bc8c75dc7aa16c436463185af3cc276
aidan <apdelany1@gmail.com>
2023-02-04 13:55:17 -0500

    added some logic to reset board, also added array with 9 possible values, all of the values represent tic tac toe board. started the isWon function which will check the scoreboard every time to see if there is a winner, i cant help but feel that there is a better way to go about checking for win, but this will do for now.

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 let marker = "X";
-
+let board = [];
 const mark = document.querySelectorAll(".cell");
 
 mark.forEach((element) => {
   element.addEventListener("click", () => {
-    console.log("hello");
     if (element.textContent === "") {
       element.textContent = marker;
       if (marker === "X") {
@@ -13,6 +12,50 @@ mark.forEach((element) => {
         marker = "X";
       }
     }
-
+    updateScore()
+    isWon();
   });
 });
+
+
+const gameBoard = {
+  board: board = ["", "", "", "", "", "", "", "", ""]
+}
+
+const players = {
+  player1: "Aidan",
+  player2: "Joe"
+}
+
+function updateScore(){
+  counter = 0;
+  mark.forEach((element) => {
+    board[counter] = element.textContent;
+    counter++;
+  })
+console.log(board);
+}
+
+
+const h = document.querySelector("h1")
+
+h.addEventListener("click", () => {
+  mark.forEach((element) => {
+      element.textContent = ""
+  });
+  board = ["", "", "", "", "", "", "", "", ""]
+  marker = "X"
+  console.log(board)
+})
+ // eight possible ways to win tic tac toe. starting with the top row, we will seek winning matches. 
+function isWon() {
+  if (board[0] === board[1] && board[0] === board[2]) {
+    if (board[0] != "") {
+    console.log("win");
+    }
+  }
+}
+
+
+
+
